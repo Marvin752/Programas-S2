20b6ab8d219eaf0b034ed4b5297bd79c76bc3eca
Marvin Cámbara <marvin75205@gmail.com>
2024-02-13 17:12:14 -0600

    Lo deje mas bonito

--- Conversion de tipos/Program.cs
+++ Conversion de tipos/Program.cs
@@ -1,38 +1,38 @@
-﻿/*
+﻿//Convertir Entero a Decimal
 
 Console.Write("Ingrese un numero entero: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-decimal total = Convert.ToDecimal(num1);
+int num80 = Convert.ToInt32(Console.ReadLine());
+decimal total78 = Convert.ToDecimal(num80);
 Console.Write("Ingrese un numero decimal: ");
-decimal num2 = Convert.ToDecimal(Console.ReadLine());
-decimal num3 = num2 + total;
+decimal num20 = Convert.ToDecimal(Console.ReadLine());
+decimal num30 = num20 + total78;
 Console.WriteLine("El numero convertido a entero es: " + total);
-Console.WriteLine("Y sumado con un decimal es: " + num3);*/
+Console.WriteLine("Y sumado con un decimal es: " + num30);
 
 //Decimal a Float
 
-/*decimal num1 = 5.9999999m;
-float num2 = (float)num1;
-Console.WriteLine(num2);*/
+decimal num54 = 5.9999999m;
+float num26 = (float)num54;
+Console.WriteLine(num26);
 
-/*Convertir un Float a entero
+//Convertir un Float a entero
 
-float num1 = 5.56f;
-Console.WriteLine("El numero es: " + num1);
-int num2 = Convert.ToInt32(num1);
-Console.WriteLine("El numero ahora es: " + num2);*/
+float num14 = 5.56f;
+Console.WriteLine("El numero es: " + num14);
+int num28 = Convert.ToInt32(num14);
+Console.WriteLine("El numero ahora es: " + num28);
 
-/*Convertir caracter a entero 
+//Convertir caracter a entero 
 
-char num1 = 'B';
-int num2 = Convert.ToInt32(num1);
-Console.WriteLine(num2);*/
+char num16 = 'B';
+int num22 = Convert.ToInt32(num16);
+Console.WriteLine(num22);
 
 //Converitr entero a caracter
 
-/*int num1 = 65;
+int num1 = 65;
 char num2 = (char)num1;
-Console.WriteLine("El caracter de " + num1 + " en ASCII es: " + num2);*/
+Console.WriteLine("El caracter de " + num1 + " en ASCII es: " + num2);
 
 
 
