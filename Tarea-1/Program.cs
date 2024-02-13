0d7b73656913ac6122dae504e00d21355b29989b
Marvin Cámbara <marvin75205@gmail.com>
2024-02-12 20:51:16 -0600

    Agregar archivos de proyecto.

---
+++ Tarea-1/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+
+Console.Write("Ingrese un numero entero: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+decimal total = Convert.ToDecimal(num1);
+Console.Write("Ingrese un numero decimal: ");
+decimal num2 = Convert.ToDecimal(Console.ReadLine());
+decimal num3 = num2 + total;
+Console.WriteLine("El numero convertido a entero es: " + total);
+Console.WriteLine("Y sumado con un decimal es: " + num3);*/
+
+//Decimal a Float
+
+/*decimal num1 = 5.9999999m;
+float num2 = (float)num1;
+Console.WriteLine(num2);*/
+
+/*Convertir un Float a entero
+
+float num1 = 5.56f;
+Console.WriteLine("El numero es: " + num1);
+int num2 = Convert.ToInt32(num1);
+Console.WriteLine("El numero ahora es: " + num2);*/
+
+/*Convertir caracter a entero 
+
+char num1 = 'B';
+int num2 = Convert.ToInt32(num1);
+Console.WriteLine(num2);*/
+
+//Converitr entero a caracter
+
+/*int num1 = 65;
+char num2 = (char)num1;
+Console.WriteLine("El caracter de " + num1 + " en ASCII es: " + num2);*/
+
+
+
+
+
+
